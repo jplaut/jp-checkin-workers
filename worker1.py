82f72aae47f22f03edcc079d6eb3a8c6c5495be1
Jon Plaut <jp@tangerinesymphony.net>
2012-02-08 00:20:09 -0800

    using worker.work instead of .run

--- worker1.py
+++ worker1.py
@@ -2,4 +2,5 @@ from os import environ
 from pyres.worker import Worker
 from tasks import *
 
-Worker.run(['aggregate_checkins'], server=environ.get('REDIS_QUEUE_SERVER'), password=environ.get('REDIS_QUEUE_PASSWORD'))
+w = Worker(['aggregate_checkins'], server=environ.get('REDIS_QUEUE_SERVER'), password=environ.get('REDIS_QUEUE_PASSWORD'))
+w.work()
