f692bc7b9c4b81a23c6352af6a4f862a33061d19
Jon Plaut <jp@tangerinesymphony.net>
2012-02-07 23:01:05 -0800

    trying with normal pyres settings

--- worker1.py
+++ worker1.py
@@ -1,5 +1,5 @@
 from os import environ
-from myres.worker import Worker
+from pyres.worker import Worker
 from tasks import *
 
-Worker.run(['aggregate_checkins'], server=environ.get('REDIS_QUEUE_SERVER'), password=environ.get('REDIS_QUEUE_PASSWORD'))
+Worker.run(['aggregate_checkins'], server=environ.get('REDIS_QUEUE_SERVER'))
