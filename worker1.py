9fa1b7cb53bbe38975f878b4f6db0a8cfecc68bc
Jon Plaut <jp@tangerinesymphony.net>
2012-02-08 00:23:22 -0800

    using worker.work instead of .run

--- worker1.py
+++ worker1.py
@@ -1,6 +1,15 @@
 from os import environ
 from pyres.worker import Worker
+from pyres import ResQ
+import redis
 from tasks import *
 
-w = Worker(['aggregate_checkins'], server=environ.get('REDIS_QUEUE_SERVER'), password=environ.get('REDIS_QUEUE_PASSWORD'))
+redisHost = environ.get("REDIS_QUEUE_HOST")
+redisPort = int(environ.get("REDIS_QUEUE_PORT"))
+redisPassword = environ.get("REDIS_QUEUE_PASSWORD")
+
+redisObject = redis.Redis(host=redisHost, port=redisPort, password=redisPassword)
+
+r = ResQ(redisObject)
+w = Worker(r)
 w.work()
