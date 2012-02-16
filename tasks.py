6b3b5ad0f0559ddaf783b0ce5e0fb718cf6bb7b7
Jon Plaut <jp@tangerinesymphony.net>
2012-02-15 23:42:00 -0800

    got everything up and running. now to see if it pulls in all checkins

--- tasks.py
+++ tasks.py
@@ -29,7 +29,7 @@ def fql(fql, token, args=None):
 		args = {}
 	
 	args["q"], args["format"], args["access_token"] = fql, "json", token
-
+		
 	return json.loads(
 		urllib2.urlopen("https://graph.facebook.com/fql?" +
 						urllib.urlencode(args)).read())
@@ -47,7 +47,7 @@ class GetFriends:
 	def perform(user, offset, limit, token):
 		friendsArray = []
 		
-		friendsRaw = fql("SELECT uid2 FROM friend WHERE uid1=me()", token, args={'limit':limit, 'offset':offset})
+		friendsRaw = fql("SELECT uid2 FROM friend WHERE uid1=me() LIMIT %s OFFSET %s" % (limit, offset), token)
 		
 		for friend in friendsRaw['data']:
 			friendsArray.append(friend['uid2'])
@@ -66,17 +66,20 @@ class AggregateCheckins:
 		batch = ""
 		for friend in friends:
 			#while friends.index(friend) != len(friends)-1:
-			batch += "{'method':'GET','relative_url':'%s/checkins'}," % friend
+			batch += "{'method':'GET','relative_url':'%s/checkins?limit=3000'}," % friend
 			#batch += "{'method':'GET','relative_url':'%s/checkins'}" % friend
-		payload = {'batch':'[%s]' % batch, 'method':'post','access_token':token, 'limit':3000}
-
-		r = requests.post(baseURL, data=payload)
-		dataJSON = json.loads(r.text)
-
-		for call in dataJSON:
-			checkins = ast.literal_eval(call['body'])['data']
-			redisQueue.enqueue(MoveCheckinsToDatabase, checkins, user)
+		payload = {'batch':'[%s]' % batch, 'method':'post','access_token':token}
 		
+		r = requests.post(baseURL, data=payload)
+		redisObject.set("request", r.text)
+		
+		dataJSON = json.loads(r.text)
+		
+		
+		for person in dataJSON:
+			checkins=json.loads(person['body'])['data']
+			redisQueue.enqueue(MoveCheckinsToDatabase, checkins, user)
+			
 				
 class MoveCheckinsToDatabase:
 	
@@ -85,9 +88,13 @@ class MoveCheckinsToDatabase:
 	@staticmethod
 	def perform(checkins, user):
 		checkin_metadata = {}
+		collection = db[user]
 		
 		for checkin in checkins:
-			if 'id' in checkin:
+			if collection.find_one({'checkin_id':checkin['id']}):
+				pass
+			else:
+				checkin_metadata['checkin_id'] = checkin['id']
 				if 'from' in checkin:
 					if 'name' in checkin['from']:
 						checkin_metadata['author_name'] = checkin['from']['name']
@@ -96,15 +103,18 @@ class MoveCheckinsToDatabase:
 				if 'message' in checkin:
 					checkin_metadata['comment'] = checkin['message']
 				if 'place' in checkin:
+					if 'location' in checkin['place']:
+						if 'city' in checkin['place']['location']:
+							checkin_metadata['city'] = checkin['place']['location']['city']
+						if 'country' in checkin['place']['location']:
+							checkin_metadata['country'] = checkin['place']['location']['country']
+						if 'state' in checkin['place']['location']:
+							checkin_metadata['state'] = checkin['place']['location']['state']
 					if 'id' in checkin['place']:
 						checkin_metadata['place_id'] = checkin['place']['id']
 					if 'name' in checkin['place']:
 						checkin_metadata['place_name'] = checkin['place']['name']
-					if 'location' in checkin['place']:
-						checkin_metadata['place_city'] = checkin['place']['location']
-				
-				collection = db[user]
+
 				collection.insert(checkin_metadata)
-			else:
-				pass
-	
+					
+					
